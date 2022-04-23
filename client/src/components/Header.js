430319bf10f67319464d68e3e87655192d0f498d
Simmone Kelly <simmone.kelly@gmail.com>
2022-04-23 16:47:02 -0400

    adding logic for dashbaord and new workout to check if user is logged in, made header dynamic

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,27 +1,65 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import React, { Component } from "react";
 
-function Header() {
-  return (
-    <section className="header">
-      <Link to="/">
-        <img alt="logo" className="header_logo" src="http://localhost:8080/images/lsbs-logo.png" />
-      </Link>
-      <div className="header_nav-container">
-        <ul className="header_nav">
-          <Link to="/myprofile">
-            <li>Dashboard</li>
-          </Link>
-          <Link to="/log-new-workout">
-            <li>Start New Workout</li>
-          </Link>
-          <Link to="/">
-            <li>Log Out</li>
-          </Link>
-        </ul>
-      </div>
-    </section>
-  );
+export default class Header extends Component {
+  state = {
+    isLoggedIn: true,
+  };
+
+  componentDidMount() {
+    console.log("header mounted");
+    const token = sessionStorage.getItem("token");
+    console.log(token);
+
+    if (!token) {
+      this.setState({
+        isLoggedIn: false,
+      });
+    }
+  }
+
+  componentDidUpdate() {
+    console.log("header updated");
+    const token = sessionStorage.getItem("token");
+    console.log(token);
+
+    if (token) {
+      this.setState({
+        isLoggedIn: true,
+      });
+    }
+  }
+
+  render() {
+    return (
+      <section className="header">
+        <Link to="/">
+          <img
+            alt="logo"
+            className="header_logo"
+            src="http://localhost:8080/images/lsbs-logo.png"
+          />
+        </Link>
+        <div className="header_nav-container">
+          <ul className="header_nav">
+            <Link to="/myprofile">
+              <li>Dashboard</li>
+            </Link>
+            <Link to="/log-new-workout">
+              <li>Start New Workout</li>
+            </Link>
+            {this.state.isLoggedIn ? (
+            <Link to="/">
+              <li>Log Out</li>
+            </Link>
+            ) : (
+              <Link to="/login">
+              <li>Log In</li>
+            </Link>
+            )}
+          </ul>
+        </div>
+      </section>
+    );
+  }
 }
-
-export default Header;
--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,30 +1,40 @@
 import axios from "axios";
 import React, { Component } from "react";
 import "../Dashboard/Dashboard.scss";
+import { Link } from "react-router-dom"
 
 export default class Dashboard extends Component {
   state = {
     workoutHistory: [],
     isLoading: true,
     userInfo: {},
+    isLoggedIn: true,
   };
 
   componentDidMount() {
     console.log("dashboard mounted");
     const token = sessionStorage.getItem("token");
-    axios
-      .get("http://localhost:8080/history", {
-        headers: { Authorization: `Bearer ${token}` },
-      })
-      .then((res) => {
-        this.setState({
-          isLoading: false,
-          workoutHistory: res.data.history[0].workouts,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
+
+    if (!token) {
+      this.setState({
+        isLoggedIn: false,
       });
+    } else {
+      axios
+        .get("http://localhost:8080/history", {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((res) => {
+          this.setState({
+            isLoading: false,
+            workoutHistory: res.data.history[0].workouts,
+          });
+        })
+        .catch((err) => {
+          //fix error where server crashes
+          console.log(err);
+        });
+    }
   }
 
   componentDidUpdate() {
@@ -32,45 +42,59 @@ export default class Dashboard extends Component {
   }
 
   render() {
-    
-    if (this.state.workoutHistory.length === 0) {
-      return <h1>No History</h1>;
-    }
-    return (
-      <section className="myprofile">
-        <h1>My Profile</h1>
-        <div className="myprofile_workout-container">
-          {this.state.workoutHistory.map((workout) => (
-            <table key={workout.id} className="myprofile_workout">
-              <thead>
-                <tr>
-                  <th colSpan="3">Completed On: {workout.date}</th>
-                </tr>
-              </thead>
-              {workout.exercises.map((exercise) => (
-                <tbody key={exercise.id} className="myprofile_exercise">
+    if (!this.state.isLoggedIn) {
+      return (
+        <section className="myprofile">
+          <h1>Please Log In To See Workout History</h1>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        </section>
+      );
+    } else {
+      if (this.state.workoutHistory.length === 0) {
+        return (
+          <section className="myprofile">
+          <h1>No History</h1>
+          <p>Complete your first work out and it will display here.</p>
+          </section>
+        )
+      }
+      return (
+        <section className="myprofile">
+          <h1>My Profile</h1>
+          <div className="myprofile_workout-container">
+            {this.state.workoutHistory.map((workout) => (
+              <table key={workout.id} className="myprofile_workout">
+                <thead>
                   <tr>
-                    <th colSpan="3">{exercise.name}</th>
+                    <th colSpan="3">Completed On: {workout.date}</th>
                   </tr>
-                  <tr>
-                    <td>Set</td>
-                    <td>Reps</td>
-                    <td>Weight</td>
-                  </tr>
-                  {exercise.sets.map((set) => (
-                    <tr key={set.setNumber}>
-                      <td>{`Set ${set.setNumber}`}</td>
-                      <td>{set.reps}</td>
-                      <td>{set.weight}</td>
+                </thead>
+                {workout.exercises.map((exercise) => (
+                  <tbody key={exercise.id} className="myprofile_exercise">
+                    <tr>
+                      <th colSpan="3">{exercise.name}</th>
                     </tr>
-                  ))}
-                </tbody>
-              ))}
-              
-            </table>
-          ))}
-        </div>
-      </section>
-    );
+                    <tr>
+                      <td>Set</td>
+                      <td>Reps</td>
+                      <td>Weight</td>
+                    </tr>
+                    {exercise.sets.map((set) => (
+                      <tr key={set.setNumber}>
+                        <td>{`Set ${set.setNumber}`}</td>
+                        <td>{set.reps}</td>
+                        <td>{set.weight}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                ))}
+              </table>
+            ))}
+          </div>
+        </section>
+      );
+    }
   }
 }
